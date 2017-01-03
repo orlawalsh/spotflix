1f147fa24203603ae3a00fbdd0e793e21dfda31d
Orla Walsh <orlawalsh@outlook.com>
2017-01-03 00:42:34 +0000

    Add dashboard

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,30 @@
 class SongsController < ApplicationController
+	before_action :find_song, only: [:show, :dashboard]
+	before_action :find_list, only: [:show, :dashboard]
+
 		def index
 		@songs = Song.all.order("created_at DESC")
 	end
 
 	def show
+		
+	end
+	
+	def dashboard
+	end
+
+	private
+
+	def find_song
+		if params[:id].nil?
+			@song = current_song
+		else
 		@song = Song.find(params[:id])
+	end
+
+	def find_list
 		@lists = List.where(song_id: @song).order("created_at DESC")
 	end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :songs
-root 'welcome#index'
 
 resources :songs, only: [:index, :show] do
 	resources :lists
 end
+	authenticated :song do
+		root 'songs#dashboard', as: "authenticated_root"
+	end
 
+		root 'welcome#index'
 end
