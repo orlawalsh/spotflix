a9b261d8076cb828951a3f3371b9a5c2763c648d
Orla Walsh <orlawalsh@outlook.com>
2017-01-03 02:11:59 +0000

    Add, update, delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
 	before_action :find_song
-	before_action :find_list, only: [:show]
+	before_action :find_list, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@list = @song.lists.new
@@ -21,6 +21,24 @@ class ListsController < ApplicationController
 
 	end
 
+	def edit
+
+	end
+
+	def update
+		if @list.update list_params
+			redirect_to song_list_path(@song, @list), notice: "Song updated!"
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@list.destroy
+		redirect_to root_path
+
+		
+	end
 	private 
 
 	def list_params
